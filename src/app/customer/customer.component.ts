715b2e83c07e70109f077796108e73c931b4fc96
mohamedbouhamed <med.bouhamed@live.fr>
2021-10-27 14:49:11 +0200

    styles and design

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {customer} from '../Models/customer'
+import {NgForm} from "@angular/forms";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+    selector: 'app-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
 
-Customers: Array<customer>;
-IsAddNew:boolean = false;
-FName : string = '';
-LName : string = '';
+    Customers: Array<customer>;
+    IsAddNew: boolean = false;
+    FName: string = '';
+    LName: string = '';
+    @ViewChild('myForm') CustomerForm: NgForm | undefined;
 
-  constructor() {
-  this.Customers = new Array<customer>();
-  this.Customers.push(new customer(' Mohamed','BOUHAMED'));
-  this.Customers.push(new customer(' Emna','BORGI'));
-  this.Customers.push(new customer(' Haroun','BOUHAMED'));
-   }
-   AddNew(){
-   this.IsAddNew=true;
-   }
-   save(){
-   this.Customers.push(new customer(this.FName,this.LName));
-   this.rest();
-   this.IsAddNew = false}
-   rest(){
-   this.FName = '';
-   this.LName = '';
-   this.IsAddNew = false
-   }
+    constructor() {
+        this.Customers = new Array<customer>();
+        this.Customers.push(new customer(' MOHAMED', 'BOUHAMED'));
+        this.Customers.push(new customer('ANGULAR', 'IS THE BEST'));
+        this.Customers.push(new customer('FRONTEND ', 'FRAMEWORK'));
+    }
 
-  ngOnInit(): void {
-  }
+    AddNew() {
+        this.IsAddNew = true;
+    }
+
+    save() {
+        this.Customers.push(new customer(this.FName, this.LName));
+        this.rest();
+        this.IsAddNew = false
+    }
+
+    rest() {
+        //this.FName = '';
+        //this.LName = '';
+        this.CustomerForm?.reset();
+        this.IsAddNew = false
+    }
+
+    ngOnInit(): void {
+    }
 
 }
